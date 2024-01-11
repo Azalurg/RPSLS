8d6a9a4a0820c1598d0daa15a667054b9fa534e1
Azalurg <91213389+Azalurg@users.noreply.github.com>
2024-01-11 13:13:19 +0100

    Change app design
    - main.py holds game loop now
    - Change Game to builder
    - Add more functions for engines

--- src/engines.py
+++ src/engines.py
@@ -1,15 +1,25 @@
 class Engine:
-    def __init__(self, conditions):
+    def __init__(self, conditions, options):
         self.conditions = conditions
+        self.options = options
+
+        if len(self.conditions) != len(self.options):
+            exit("Wrong game engine")
 
     def get_result(self, player: int, bot: int) -> int:
         return self.conditions[player][bot]
+
+    def print_options(self):
+        for i, option in enumerate(self.options):
+            print(f"{i+1}. {option}", end="; ")
+        print("")
 
 
 class SimpleEngine(Engine):
     def __init__(self):
         conditions = [[0, -1, 1], [1, 0, -1], [-1, 1, 0]]
-        super().__init__(conditions)
+        options = ["Rock", "Paper", "Scissors"]
+        super().__init__(conditions, options)
 
 
 class AdvanceEngine(Engine):
@@ -21,4 +31,6 @@ class AdvanceEngine(Engine):
             [-1, 1, -1, 0, 1],
             [1, -1, 1, -1, 0],
         ]
-        super().__init__(conditions)
+        options = ["Rock", "Paper", "Scissors", "Lizard", "Spock"]
+
+        super().__init__(conditions, options)
