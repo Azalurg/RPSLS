87a0c9dd944940c071439a32c1aee68c3cb934ab
Azalurg <91213389+Azalurg@users.noreply.github.com>
2024-01-11 12:29:47 +0100

    Create Bot class

---
+++ src/bot.py
@@ -0,0 +1,10 @@
+class Bot:
+    def __init__(self, options=3, seed=999983):
+        self.games_played = 0
+        self.seed = seed
+        self.options = options
+
+    def play(self) -> int:
+        self.games_played += 1
+        self.seed = (self.seed * self.games_played + 997) % 9999991
+        return self.seed % self.options
